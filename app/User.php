e610aa07b1d10f8d642876158de9246c62233e93
Lazar Djokovic <lazar.djokovic.pvt@gmail.com>
2018-11-11 19:17:50 +0100

    Add author info and question creation date in question item

--- app/User.php
+++ app/User.php
@@ -33,9 +33,9 @@ class User extends Authenticatable
         return $this->hasMany(Question::class);
     }
 
-    public function setTitleAttribute($value)
+    public function getUrlAttribute()
     {
-        $this->attributes['title']=$value;
-        $this->attributes['slug']=str_slug($value);
+        //return route("questions.show",$this->id);
+        return '#';
     }
 }
